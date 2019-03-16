e1b53202eb47dc085e2ebf16b4da4c106a528442
Vladislav Polyakov <polrkov.tm@gmail.com>
2019-03-16 22:16:18 +0300

    Update App Component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,25 @@
 import * as React from 'react'
+import { Main } from './Main'
+import { Header } from './Header'
+import { cn } from '@bem-react/classname'
+import { Footer } from './Footer'
 
-const App: React.FC = props => {
-  return <div>I can fly 3!</div>
+const appCN = cn('App')
+
+const App: React.FC = () => {
+  return (
+    <div className={appCN()}>
+      <div className={appCN('Header')}>
+        <Header />
+      </div>
+      <div className={appCN('Main')}>
+        <Main />
+      </div>
+      <div className={appCN('Footer')}>
+        <Footer />
+      </div>
+    </div>
+  )
 }
 
 export default App

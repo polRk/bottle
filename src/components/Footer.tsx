5efd97dcf6501bcb80084dfbb515b43f23a1e55c
Vladislav Polyakov <polrkov.tm@gmail.com>
2019-03-16 21:49:00 +0300

    Add Footer Component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import { cn } from '@bem-react/classname'
+
+const footerCN = cn('Footer')
+
+export const Footer: React.FC = () => {
+  return (
+    <footer className={footerCN()}/>
+  )
+}

4b672b0b4c396142f4e5f12da71c11319dc51dda
Vladislav Polyakov <polrkov.tm@gmail.com>
2019-03-17 11:18:32 +0300

    feat(search): сделал форму поиска

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -5,5 +5,23 @@ import './index.scss'
 const searchCN = cn('Search')
 
 export const Search: React.FC = () => {
-  return <div className={searchCN()} />
+  return (
+    <div className={searchCN()}>
+      <form action={'/search'} className={searchCN('Form')}>
+        <input
+          id="search"
+          type="search"
+          autoComplete="off"
+          autoCorrect="off"
+          autoCapitalize="off"
+          spellCheck={false}
+          aria-autocomplete="list"
+          className={searchCN('Input')}
+          placeholder="Поиск"
+        />
+        <label htmlFor="search" className={searchCN('Label')} />
+        <input type="submit" value="Найти" className={searchCN('Submit')} />
+      </form>
+    </div>
+  )
 }

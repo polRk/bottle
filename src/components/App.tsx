5b8ca024d8320c9b5db7a8027ab9329f19dcbc94
Vladislav Polyakov <polrkov.tm@gmail.com>
2019-03-18 01:46:53 +0300

    feat(app): перенес получение данных в этот компонент

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,16 +4,39 @@ import { Header } from './Header'
 import { cn } from '@bem-react/classname'
 import { Footer } from './Footer'
 import './index.scss'
+import { Filters } from './Filters'
+import { dispatch, useGlobalState } from '../store'
+import { FAILURE_NOTES, FETCH_NOTES, SUCCESS_NOTES } from '../store/constants'
+import { useEffect } from 'react'
 
 const appCN = cn('App')
 
+interface IResponse {
+  notes: []
+  colors: []
+  tags: []
+}
+
 const App: React.FC = () => {
+  useEffect(() => {
+    dispatch({ type: FETCH_NOTES })
+    fetch(
+      'https://gist.githubusercontent.com/shri-minsk-2019-reviewer/52e831aa051046aca6a493367ee83d2c/raw/c95129963a895d11f791a7830eead8d4dcb9182f/shri.json'
+    )
+      .then(resp => resp.json())
+      .then((data: IResponse) =>
+        dispatch({ type: SUCCESS_NOTES, payload: data })
+      )
+      .catch(error => dispatch({ type: FAILURE_NOTES, message: error }))
+  }, [])
+
   return (
     <div className={appCN()}>
       <div className={appCN('Header')}>
         <Header />
       </div>
       <div className={appCN('Main')}>
+        <Filters />
         <Main />
       </div>
       <div className={appCN('Footer')}>

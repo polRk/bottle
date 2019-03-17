0d4e1e81dba287f273c523388230785d78714bca
Vladislav Polyakov <polrkov.tm@gmail.com>
2019-03-17 11:19:24 +0300

    feat(header): подключил логотип и поиск к header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react'
 import { cn } from '@bem-react/classname'
 import './index.scss'
+import { Logo } from '../Logo'
+import { Search } from '../Search'
 
 const headerCN = cn('Header')
 
 export const Header: React.FC = () => {
-  return <header className={headerCN()}>Hello World</header>
+  return (
+    <header className={headerCN()}>
+      <Logo />
+      <Search />
+    </header>
+  )
 }

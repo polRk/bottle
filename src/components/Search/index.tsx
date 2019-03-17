ae682c874969b2d6e5262c3887d0f2374c5fae38
Vladislav Polyakov <polrkov.tm@gmail.com>
2019-03-18 01:45:33 +0300

    feat(search): добавил автофокус на поле поиска

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,10 +1,19 @@
 import * as React from 'react'
 import { cn } from '@bem-react/classname'
 import './index.scss'
+import { useRef, useEffect } from 'react'
 
 const searchCN = cn('Search')
 
 export const Search: React.FC = () => {
+  const searchRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    if (searchRef.current) {
+      searchRef.current.focus()
+    }
+  }, [searchRef.current])
+
   return (
     <div className={searchCN()}>
       <form action={'/search'} className={searchCN('Form')}>
@@ -18,6 +27,7 @@ export const Search: React.FC = () => {
           aria-autocomplete="list"
           className={searchCN('Input')}
           placeholder="Поиск"
+          ref={searchRef}
         />
         <label htmlFor="search" className={searchCN('Label')} />
         <input type="submit" value="Найти" className={searchCN('Submit')} />

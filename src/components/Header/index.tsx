f4f0f6b101d4c1b109772347446ac62f2f3ba774
Vladislav Polyakov <polrkov.tm@gmail.com>
2019-03-18 00:17:54 +0300

    feat(header): теперь header выглядит как в шаблоне

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,14 +3,20 @@ import { cn } from '@bem-react/classname'
 import './index.scss'
 import { Logo } from '../Logo'
 import { Search } from '../Search'
+import { Navigation } from '../Navigation'
 
 const headerCN = cn('Header')
 
 export const Header: React.FC = () => {
   return (
     <header className={headerCN()}>
-      <Logo />
-      <Search />
+      <div className={headerCN('Logo')}>
+        <Logo />
+      </div>
+      <div className={headerCN('Navigation')}>
+        <Search />
+        <Navigation />
+      </div>
     </header>
   )
 }

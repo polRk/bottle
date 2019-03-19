3374b4e9d52cbb680269ba3affac68dd71a25c52
Vladislav Polyakov <polrkov.tm@gmail.com>
2019-03-20 00:42:55 +0300

    feat: создал типы для заметок

--- src/shared.ts
+++ src/shared.ts
@@ -8,32 +8,35 @@ export interface IColor {
   color: string
 }
 
-export interface INote {
+export interface INoteBase {
   created: number
   title?: string
   attachments?: Array<NoteAttachment>
   size?: NoteSize
   reminder?: number
+  color?: number
+  tags?: number[]
 }
 
-export interface INoteText extends INote {
+export interface INoteText extends INoteBase {
   type: NoteType.text
   text: string
 }
 
-export interface INoteList extends INote {
+export interface INoteList extends INoteBase {
   type: NoteType.list
   items: Array<{ text: string; checked: boolean }>
 }
 
-export interface INoteImage extends INote {
+export interface INoteImage extends INoteBase {
   type: NoteType.image
   url: string
 }
 
 export enum NoteAttachmentType {
-  'link',
-  'image',
+  link = 'link',
+  image = 'image',
+  sep = 'sep',
 }
 
 export interface INoteAttachmentLink {
@@ -46,18 +49,31 @@ export interface INoteAttachmentImage {
   url: string
 }
 
-export type NoteAttachment = INoteAttachmentLink | INoteAttachmentImage
+export type NoteAttachment =
+  | INoteAttachmentLink
+  | INoteAttachmentImage
+  | { type: NoteAttachmentType.sep }
 
 export enum NoteType {
-  'list',
-  'text',
-  'image',
+  list = 'list',
+  text = 'text',
+  image = 'image',
 }
 
 export enum NoteSize {
-  's',
-  'm',
-  'l',
+  s = 's',
+  m = 'm',
+  l = 'l',
 }
 
 export type Note = INoteText | INoteList | INoteImage
+
+export const hex2rgba = (hex: string, alpha = 1) => {
+  const rgb = hex.match(/\w\w/g)
+  if (rgb) {
+    const [r, g, b] = rgb.map(x => parseInt(x, 16))
+    return `rgba(${r},${g},${b},${alpha})`
+  } else {
+    return undefined
+  }
+}

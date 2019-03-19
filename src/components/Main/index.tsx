94c4d3b11d1fba35c04f5745813069c744487134
Vladislav Polyakov <polrkov.tm@gmail.com>
2019-03-20 00:36:03 +0300

    feat(main): сделал рендеринг заметок

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -2,19 +2,17 @@ import * as React from 'react'
 import { cn } from '@bem-react/classname'
 import { useGlobalState } from '../../store'
 import './index.scss'
+import { NoteWrapper } from '../Note/NoteWrapper'
 
 const mainCN = cn('Main')
 
 export const Main: React.FC = () => {
   const [notes] = useGlobalState('notes')
-
   return (
     <main className={mainCN()}>
-      <ul>
-        {notes.map(note => (
-          <li key={note.created}>{note.title}</li>
-        ))}
-      </ul>
+      {notes.map(note => (
+        <NoteWrapper note={note} key={note.created} />
+      ))}
     </main>
   )
 }

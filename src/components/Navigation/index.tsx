b74dd02c01e63979976de6bf3ffc42206f378d54
Vladislav Polyakov <polrkov.tm@gmail.com>
2019-03-20 10:03:21 +0300

    feat(navigation): добавил иконку раскрытия меню

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -8,11 +8,29 @@ const navigationCN = cn('Navigation')
 export const Navigation: React.FC = () => {
   return (
     <nav className={navigationCN()}>
-      <ButtonGroup>
-        <Button>Активные</Button>
-        <Button appearance={'link'}>Архив</Button>
-        <Button appearance={'primary'}>Добавить</Button>
-      </ButtonGroup>
+      <div className={navigationCN('Links')}>
+        <ButtonGroup>
+          <Button>Активные</Button>
+          <Button appearance={'link'}>Архив</Button>
+          <Button appearance={'primary'}>Добавить</Button>
+        </ButtonGroup>
+      </div>
+      <div className={navigationCN('Expand')}>
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M1 6H23V4H1V6ZM1 13H23V11H1V13ZM23 20H1V18H23V20Z"
+            fill="black"
+          />
+        </svg>
+      </div>
     </nav>
   )
 }

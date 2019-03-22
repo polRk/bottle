76cda2f6c49a472b8b610ecc77e32645a04a9dd2
Vladislav Polyakov <polrkov.tm@gmail.com>
2019-03-22 22:08:45 +0300

    feat(notes): написал класс для работы с заметками

--- src/shared.ts
+++ src/shared.ts
@@ -1,3 +1,5 @@
+import { unionBy } from 'lodash'
+
 export interface ITag {
   id: number
   tag: string
@@ -10,6 +12,8 @@ export interface IColor {
 
 export interface INoteBase {
   created: number
+  id?: number
+  archived?: boolean
   title?: string
   attachments?: Array<NoteAttachment>
   size?: NoteSize
@@ -67,6 +71,125 @@ export enum NoteSize {
 }
 
 export type Note = INoteText | INoteList | INoteImage
+
+const random = () => Math.ceil(Math.random() * 10000)
+
+export class Notes implements Iterable<Note> {
+  private readonly notes: Note[] = []
+
+  constructor(notes: Note[]) {
+    this.notes = [...notes]
+  }
+
+  static factory(notes: Note[]): Note[] {
+    return notes.map(note => {
+      return {
+        ...note,
+        id: random(),
+        size: note.size ? note.size : NoteSize.m,
+      }
+    })
+  }
+
+  private getnoteSize(note: Note): NoteSize {
+    switch (note.type) {
+      case NoteType.list:
+        return NoteSize.l
+      case NoteType.image:
+        return NoteSize.m
+      case NoteType.text:
+        return NoteSize.s
+      default:
+        return NoteSize.m
+    }
+  }
+
+  addNote(note: Note, archived = false): Note {
+    const newNote = {
+      ...note,
+      id: random(),
+      archived,
+      size: note.size ? note.size : this.getnoteSize(note),
+    }
+
+    this.push(newNote)
+    return newNote
+  }
+
+  deleteNote(id: number): boolean {
+    const ind = this.notes.findIndex(note => note.id === id)
+    if (ind > -1) {
+      this.notes.splice(ind, 1)
+      return true
+    }
+    return false
+  }
+
+  updateNote(id: number, note: Note): boolean {
+    const ind = this.notes.findIndex(note => note.id === id)
+    if (ind > -1) {
+      delete note.id
+      this.notes[ind] = { ...note, id }
+      return true
+    }
+    return false
+  }
+
+  toArray(): Note[] {
+    return [...this.notes]
+  }
+
+  push(...notes: Note[]): number {
+    return this.notes.push(...notes)
+  }
+
+  entries() {
+    return this.notes.entries()
+  }
+
+  map<U>(
+    callbackfn: (value: Note, index: number, array: Notes) => U,
+    thisArg?: any
+  ): U[] {
+    const result: U[] = []
+
+    for (const [index, note] of this.entries()) {
+      result.push(callbackfn(note, index, new Notes(this.toArray())))
+    }
+
+    return result
+  }
+
+  foreach(
+    callbackfn: (value: Note, index: number, array: Notes) => void,
+    thisArg?: any
+  ): void {
+    for (const [index, note] of [...this.notes].entries()) {
+      callbackfn(note, index, new Notes(this.toArray()))
+    }
+  }
+
+  filter(
+    callbackfn: (value: Note, index: number, array: Notes) => boolean
+  ): Notes {
+    const result: Note[] = []
+
+    for (const [index, note] of [...this.notes].entries()) {
+      const rb = callbackfn(note, index, new Notes(this.toArray()))
+      if (rb) {
+        result.push(note)
+      }
+    }
+
+    return new Notes(Notes.factory(result))
+  }
+
+  *[Symbol.iterator]() {
+    for (const note of this.notes) {
+      yield note
+    }
+  }
+}
 
 export const hex2rgba = (hex: string, alpha = 1) => {
   const rgb = hex.match(/\w\w/g)
